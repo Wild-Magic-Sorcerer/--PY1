8ddc37cb7957ec097e6626d6a2134e8421e34ab7
Сергей Горелов <Infinity2573@gmail.com>
2022-11-01 15:07:49 +0300

    Поменял делитель

--- Лабораторная работа 4/task2.py
+++ Лабораторная работа 4/task2.py
@@ -12,16 +12,19 @@ def get_count_char(str_):
     return dic
 
 def get_proc_char(dic_):
+    zum = 0
+    for i, j in dic_.items():
+        zum += j
     for i in dic_:
-        dic_[i] = str(round(dic_[i]/len(dic_), 5) * 100) + "%"
+        dic_[i] = str(round(dic_[i]/zum * 100, 3)) + "%"
     return dic_
-
 
 main_str = """
     Данное предложение будет разбиваться на отдельные слова.
     В качестве разделителя для встроенного метода split будет выбран символ пробела. На выходе мы получим список отдельных слов.
     Далее нужно отсортировать слова в алфавитном порядке, а после сортировки склеить их с помощью метода строк join. Приступим!!!!
 """
+
 print(get_count_char(main_str))
 
 print(get_proc_char(get_count_char(main_str)))
